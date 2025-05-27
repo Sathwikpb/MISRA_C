b0d63c38c66a1225078b0cf017641d4264420e3c
SathwikPb <sathwik11week@gmail.com>
2025-05-27 10:00:07 +0530

    Hello World

---
+++ misra_c_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// std i/o header file
+// MISRA C:2012 Rule 8.7 - Use of 'printf' function
+int main (void)
+{
+    printf("Hello, World!\n");
+    return 0;
+
+}
+
+/* 
+char -1 byte 
+short 2 bytes
+int 4 bytes
+long 4 bytes
+long long 8 bytes   
+
+
+*/ 
